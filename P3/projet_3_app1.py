8a012ddcc633fe5047e12232effc1a0a7a44d7fd
HASAN BUYUK <hasan.buyuk@nomachine.univ-eiffel.fr>
2025-12-01 12:36:47 +0100

    ajout de couleurs pour chaque département

--- P3/projet_3_app1.py
+++ P3/projet_3_app1.py
@@ -1,7 +1,7 @@
 import fltk
 import shapefile
 import math
-
+import random
 sf = shapefile.Reader("departement_50m/departements-20140306-50m",  encoding="latin1")
 
 dico_info_departement = {}
@@ -45,10 +45,10 @@ def deg_to_mercator(dico_coord):
             
             y_mercator_ajuste = y_mercator * C_AMPLIFICATION 
 
-            # Remplacer la ligne initiale par la nouvelle coordonnée projetée et ajustée
+            #nouvelle coordonnée projetée et ajustée
             liste_coord_mercator.append((lon_mercator, y_mercator_ajuste))
             
-            #Mise à jour des bornes
+            #bornes
             x_min_all = min(x_min_all, lon_mercator)
             x_max_all = max(x_max_all, lon_mercator)
             y_min_all = min(y_min_all, y_mercator_ajuste)
@@ -57,11 +57,11 @@ def deg_to_mercator(dico_coord):
 
         dico_coord_mercator[k] = liste_coord_mercator
         
-    # La fonction retourne le dictionnaire ET les bornes globales
+    #dictionnaire et bornes globales
     return dico_coord_mercator, x_min_all, x_max_all, y_min_all, y_max_all
 
 dico_coord_mercator, x_min, x_max, y_min, y_max = deg_to_mercator(dico_info_departement)
-FENETRE_TAILLE = 1000
+FENETRE_TAILLE = 500
 MARGE = 0.9
 screen_center = FENETRE_TAILLE / 2
 
@@ -69,7 +69,7 @@ x_range = x_max - x_min
 y_range = y_max - y_min
 scale_factor = (FENETRE_TAILLE * MARGE) / max(x_range, y_range)
 
-# 3. Calcul du centre des données (point de pivot de la carte)
+#Calcul du centre des données (point de pivot de la carte)
 map_center_x = x_min + x_range / 2
 map_center_y = y_min + y_range / 2
 
@@ -98,5 +98,5 @@ for k, liste_mercator in dico_coord_mercator.items():
 
 fltk.cree_fenetre(FENETRE_TAILLE, FENETRE_TAILLE)
 for k,v in dico_coord_screen.items() :
-        fltk.polygone(v, couleur='black', remplissage='lightgreen', epaisseur=1)
+        fltk.polygone(v, couleur='black', remplissage=random.choice(["red", "orange", "yellow", "green", "blue", "indigo", "violet"]), epaisseur=1)
 fltk.attend_ev()
