92d9ab73a1068e4942b0d07b9bc52db61fab62b7
HASAN BUYUK <hasan.buyuk@nomachine.univ-eiffel.fr>
2025-12-12 12:24:32 +0100

    Grosse maj(carte + légende correctement dessiner et carte interactif avec points rouges

--- P3/projet_3_app1.py
+++ P3/projet_3_app1.py
@@ -5,7 +5,7 @@ import random
 import sys
 
 DEPARTEMENTS_A_EXCLURE = ["Guyane", "Martinique", "Guadeloupe", "La Réunion", "Mayotte"]
-FENETRE_TAILLE = 750
+FENETRE_TAILLE = 1000
 MARGE = 0.9
 DEGRADE_COULEUR = [
     "#440154", "#45095E", "#461167", "#471971", 
@@ -18,6 +18,10 @@ DEGRADE_COULEUR = [
     "#67D66A", "#75D95C", "#84D44B", "#93D638", 
     "#A3D823", "#B3DA11", "#C3DC04", "#D3DE00", 
     "#E2DF04", "#F1E018", "#FEE033", "#FDE725"]
+DICO_SEGMENT = {}
+COORDONEE_CENTRE_CERCLES = {}
+
+
 def normaliser_nom_departement(nom):
     remplacements = {
         'é': 'e', 'è': 'e', 'ê': 'e', 'ë': 'e',
@@ -42,15 +46,18 @@ def normaliser_nom_departement(nom):
     nom_final = "".join(liste_caracteres)
     return nom_final.lower()
     
+
 def recuperer_csv(fichier_csv):
     dico_stat = {}
     with open(fichier_csv, mode='r', newline='', encoding='latin-1') as fichier:
         fichier.readline()
-        for i in range(0,95) :
+        for i in range(0,96) :
             ligne = fichier.readline().strip().split(',')
             nom_normalise = normaliser_nom_departement(ligne[0])
             dico_stat[nom_normalise] = ligne[1:4]
     return dico_stat
+
+
 def etape1_chargement_et_filtrage(dico_stat):
     try :
         sf = shapefile.Reader("departement_50m/departements-20140306-50m", encoding="latin1")
@@ -64,8 +71,12 @@ def etape1_chargement_et_filtrage(dico_stat):
         nom_departement_normalise = normaliser_nom_departement(nom_departement_shape)
         if nom_departement_normalise not in DEPARTEMENTS_A_EXCLURE_NORMALISES: 
             if nom_departement_normalise in dico_stat:
-                dico_info_departement_filtre[nom_departement_shape] = [shape_record.shape.points , dico_stat[nom_departement_normalise][0]]           
+                dico_info_departement_filtre[nom_departement_shape] = [shape_record.shape.points , dico_stat[nom_departement_normalise][0] , shape_record.shape.bbox]
+                DICO_SEGMENT[nom_departement_shape] = shape_record.shape.parts
+    
     return dico_info_departement_filtre
+
+
 def etape2_projection_mercator(dico_coord):
     dico_coord_mercator = {}
     x_min_all, x_max_all = float('inf'), float('-inf')
@@ -82,9 +93,16 @@ def etape2_projection_mercator(dico_coord):
             x_max_all = max(x_max_all, x_mercator)
             y_min_all = min(y_min_all, y_mercator)
             y_max_all = max(y_max_all, y_mercator)
+
+        x_min_mercator = math.radians(liste_info_dep[2][0])
+        x_max_mercator = math.radians(liste_info_dep[2][2])
+        y_min_mercator = math.log(math.tan((math.pi / 4) + (math.radians(liste_info_dep[2][1]) / 2))) 
+        y_max_mercator = math.log(math.tan((math.pi / 4) + (math.radians(liste_info_dep[2][3]) / 2))) 
+
         pib_value = liste_info_dep[1]
-        dico_coord_mercator[k] = [liste_coord_mercator, pib_value]
+        dico_coord_mercator[k] = [liste_coord_mercator, pib_value , [(x_min_mercator , y_min_mercator) , (x_max_mercator , y_max_mercator)]]
     return dico_coord_mercator, x_min_all, x_max_all, y_min_all, y_max_all
+
 def etape3_transformation_en_ecran(dico_coord_mercator, x_min, x_max, y_min, y_max):
     centre_fenetre_en_pixel = FENETRE_TAILLE / 2
     amplitude_x = x_max - x_min
@@ -100,6 +118,7 @@ def etape3_transformation_en_ecran(dico_coord_mercator, x_min, x_max, y_min, y_m
     for k, liste_mercator_info in dico_coord_mercator.items():
         liste_mercator = liste_mercator_info[0]
         pib_value = liste_mercator_info[1]
+        bbox_mercator = liste_mercator_info[2]
         liste_coord_screen = []
         for lon_mercator, y_mercator in liste_mercator:
             vecteur_x_en_pixel = (lon_mercator - centre_x_mercator) * coefficient_ajustement
@@ -107,8 +126,17 @@ def etape3_transformation_en_ecran(dico_coord_mercator, x_min, x_max, y_min, y_m
             coordonnée_x_pixel = int(centre_fenetre_en_pixel + vecteur_x_en_pixel)
             coordonée_y_pixel = int(centre_fenetre_en_pixel - vecteur_y_en_pixel) 
             liste_coord_screen.append((coordonnée_x_pixel, coordonée_y_pixel))
-        dico_coord_screen[k] = [liste_coord_screen, pib_value] 
+        (x_min_merc, y_min_merc), (x_max_merc, y_max_merc) = bbox_mercator
+        x_min_pix = int(centre_fenetre_en_pixel + (x_min_merc - centre_x_mercator) * coefficient_ajustement)
+        x_max_pix = int(centre_fenetre_en_pixel + (x_max_merc - centre_x_mercator) * coefficient_ajustement)
+        y_min_pix = int(centre_fenetre_en_pixel - (y_max_merc - centre_y_mercator) * coefficient_ajustement)
+        y_max_pix = int(centre_fenetre_en_pixel - (y_min_merc - centre_y_mercator) * coefficient_ajustement)
+        bbox_screen = [(x_min_pix, y_min_pix), (x_max_pix, y_max_pix)]
+        
+        dico_coord_screen[k] = [liste_coord_screen, pib_value, bbox_screen] 
     return dico_coord_screen
+
+
 def etape3_5_calcul_indices(dico_coord_screen):
     liste_pib_entiers = []
     for liste_info in dico_coord_screen.values():
@@ -132,26 +160,60 @@ def etape3_5_calcul_indices(dico_coord_screen):
         indice_couleur = int(ratio * (N - 1))
         liste_info.append(indice_couleur) 
     return dico_coord_screen
+
+
 def etape4_dessiner_carte(dico_coord_screen):
     fltk.cree_fenetre(FENETRE_TAILLE, FENETRE_TAILLE)
-    for liste_info in dico_coord_screen.values() :
-        coordonnees_ecran = liste_info[0]
-        indice_couleur = liste_info[2]
+    for key , liste_info in dico_coord_screen.items() :
+        if len(DICO_SEGMENT[key]) <= 1 :
+            coordonnees_ecran = liste_info[0]
+            indice_couleur = liste_info[3]
         
-        couleur_remplissage = DEGRADE_COULEUR[indice_couleur]
-        
-        fltk.polygone(
-            coordonnees_ecran, 
-            couleur='black', 
-            remplissage=couleur_remplissage,
-            epaisseur=1
-        )
+            couleur_remplissage = DEGRADE_COULEUR[indice_couleur]
+            
+            fltk.polygone(
+                coordonnees_ecran, 
+                couleur='black', 
+                remplissage=couleur_remplissage,
+                epaisseur=1
+            )
+
+        else : 
+            coordonnees_ecran = liste_info[0]
+            indice_couleur = liste_info[3]
+            couleur_remplissage = DEGRADE_COULEUR[indice_couleur]
+            zone = DICO_SEGMENT[key]
+            for i in range(0 , len(zone)-1) : 
+                
+                fltk.polygone(
+                coordonnees_ecran[zone[i]:zone[i+1]-1], 
+                couleur='black', 
+                remplissage=couleur_remplissage,
+                epaisseur=1
+            )
+            fltk.polygone(
+                coordonnees_ecran[zone[-1]:], 
+                couleur='black', 
+                remplissage=couleur_remplissage,
+                epaisseur=1
+            )
+        for key , liste_info in dico_coord_screen.items() :
+            x_cercle = (liste_info[2][1][0] + liste_info[2][0][0])/2
+            y_cercle = (liste_info[2][1][1] + liste_info[2][0][1])/2
+            fltk.cercle(x_cercle , y_cercle , FENETRE_TAILLE * 0.00750 , couleur="red" , remplissage="red") 
+            COORDONEE_CENTRE_CERCLES[key] = [x_cercle , y_cercle]
+
+        fltk.mise_a_jour()
+            
+
 def dessiner_pays(dico_stat) :
     dico_info_departement = etape1_chargement_et_filtrage(dico_stat)
     dico_coord_mercator, x_min_all, x_max_all, y_min_all, y_max_all = etape2_projection_mercator(dico_info_departement)
     dico_coord_screen = etape3_transformation_en_ecran(dico_coord_mercator, x_min_all, x_max_all, y_min_all, y_max_all)
     dico_coord_screen_colore = etape3_5_calcul_indices(dico_coord_screen) 
     etape4_dessiner_carte(dico_coord_screen_colore)
+
+
 def dessiner_indicateur(dico_coord_screen_colore) :
     liste_pib_entiers = []
     for liste_info in dico_coord_screen_colore.values():
@@ -181,6 +243,8 @@ def dessiner_indicateur(dico_coord_screen_colore) :
         i += 1
     if (N - 1) % 5 != 0:
         fltk.texte(bord_1_x - round(FENETRE_TAILLE * 0.045) , FENETRE_TAILLE - 20, str(max_pib), couleur="black", taille=round(FENETRE_TAILLE * 0.015))
+
+
 def dessiner_pays(dico_stat) :
     dico_info_departement = etape1_chargement_et_filtrage(dico_stat)
     dico_coord_mercator, x_min_all, x_max_all, y_min_all, y_max_all = etape2_projection_mercator(dico_info_departement)
@@ -188,9 +252,41 @@ def dessiner_pays(dico_stat) :
     dico_coord_screen_colore = etape3_5_calcul_indices(dico_coord_screen) 
     etape4_dessiner_carte(dico_coord_screen_colore)
     return dico_coord_screen_colore
+
+def dessiner_zone_info() :
+    fltk.rectangle(0 , 0 , FENETRE_TAILLE * 0.20 , FENETRE_TAILLE * 0.20)
+
+def afficher_info(x_clic , y_clic , dico_coord_screen_colore) :
+    for key , coordonee in COORDONEE_CENTRE_CERCLES.items() :
+        result_x = abs(x_clic - coordonee[0])
+        result_y = abs(y_clic - coordonee[1])
+        if (result_x <= FENETRE_TAILLE * 0.00750) and (result_y <= FENETRE_TAILLE * 0.00750) :
+            fltk.texte(FENETRE_TAILLE * 0.00750 , FENETRE_TAILLE * 0.00750 , key , taille=round(FENETRE_TAILLE * 0.015) , tag="bonjour")
+            fltk.texte(FENETRE_TAILLE * 0.00750 , FENETRE_TAILLE * 0.1 , dico_coord_screen_colore[key][1] , taille=round(FENETRE_TAILLE * 0.015) , tag = "Toto")
 def main():
     info = recuperer_csv("Pib_par_habitant.csv")
     dico_colore = dessiner_pays(info)
     dessiner_indicateur(dico_colore)
-    fltk.attend_ev()
+    dessiner_zone_info()
+    while True:
+        ev = fltk.donne_ev()
+        tev = fltk.type_ev(ev)
+
+        if tev == "ClicGauche":
+            print("Clic gauche au point", (fltk.abscisse(ev), fltk.ordonnee(ev)))
+            fltk.efface("bonjour")
+            fltk.efface("Toto")
+            afficher_info(fltk.abscisse(ev) , fltk.ordonnee(ev) , dico_colore)
+        elif tev == 'Quitte':
+            break
+
+        else:
+            pass
+
+        fltk.mise_a_jour()
+
+    fltk.ferme_fenetre()
+
+
+
 main()
